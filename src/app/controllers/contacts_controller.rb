cdac3ad28e0b4773e743dd158b96e27acb863840
haku1217 <haruki12.17birthday@gmail.com>
2019-09-07 15:56:12 +0900

    sample view

--- src/app/controllers/contacts_controller.rb
+++ src/app/controllers/contacts_controller.rb
@@ -4,5 +4,25 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to @contact
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def index
+    @contact = Contact.all
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:request_user_id, :shop_id, :menu_id,
+      :comment, :fee, :request_receive_time)
+    end
 end
