80f3b2237cd5fc321a21ff66e82fab11067731bf
manami <matsubara.manami@lancers.co.jp>
2019-09-07 17:50:52 +0900

    Change root path

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'contacts/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :contacts
-  root to: 'user_shops#index'
+  root to: 'shops#index'
   resources :shops
+  resources :user_shops
 end
